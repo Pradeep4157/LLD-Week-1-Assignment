a4f2b24c78451eab56c45f4860e4d7856f3b8585
Pradeep4157 <krp18539@gmail.com>
2026-04-08 18:01:08 +0530

    partially implemented the methods..

--- Cpp/01-invoice-src-ocp.cpp
+++ Cpp/01-invoice-src-ocp.cpp
@@ -9,13 +9,6 @@
 
 using namespace std;
 
-struct LineItem
-{
-    string sku;
-    int quantity{0};
-    double unitPrice{0.0};
-};
-
 /*
 process function:
 
@@ -25,13 +18,28 @@ process function:
 
     email is also sent here only using string email..
 
+    so Responsibility of InvoiceService should be only 1..
 
+    currently the responsibilities are :
 
+    1) tax calculation. ( i removed that..)
+
+    2) applying the type of discount. (separate class created for this as well..)
+
+    3) sum calculation ( i dont think that this one needs to be done with some diff class because it is always going to be just addition)
+
+    4) printing .. this also can have diff methods.., currently it uses PDF with
 
 
 
 
 */
+struct LineItem
+{
+    string sku;
+    int quantity{0};
+    double unitPrice{0.0};
+};
 class TaxCalculator
 {
 public:
@@ -51,6 +59,61 @@ public:
     double taxFinder(double amount) override
     {
         return 0.10 * amount;
+    }
+};
+class Discount
+{
+public:
+    virtual double apply(double subtotal) = 0;
+};
+class FlatDiscount : public Discount
+{
+public:
+    FlatDiscount(double discount_) : discount(discount_) {};
+    double apply(double subtotal) override
+    {
+        return discount;
+    }
+
+private:
+    double discount;
+};
+class PercentDiscount : public Discount
+{
+public:
+    PercentDiscount(double percent_) : percent(percent_) {};
+    double apply(double subtotal) override
+    {
+        return (subtotal * (percent / 100.0));
+    }
+
+private:
+    double percent;
+};
+class InvoiceRenderer
+{
+public:
+    virtual string render(vector<LineItem> &items_, double subTotal_, double discount_, double tax_, double grand_) = 0;
+};
+class PdfRenderer : public InvoiceRenderer
+{
+public:
+    PdfRenderer() {}
+
+    string render(vector<LineItem> &items, double subTotal, double discount, double tax, double grand) override
+    {
+        // rendering inline (pretend PDF)
+        ostringstream pdf;
+        pdf << "INVOICE\n";
+        for (auto &it : items)
+        {
+            pdf << it.sku << " x" << it.quantity << " @ " << it.unitPrice << "\n";
+        }
+        pdf << "Subtotal: " << subTotal << "\n"
+            << "Discounts: " << discount << "\n"
+            << "Tax: " << tax << "\n"
+            << "Total: " << grand << "\n";
+        return pdf.str();
     }
 };
 class InvoiceService
@@ -89,18 +152,6 @@ public:
         double tax = (subtotal - discount_total) * 0.18;
         double grand = subtotal - discount_total + tax;
 
-        // rendering inline (pretend PDF)
-        ostringstream pdf;
-        pdf << "INVOICE\n";
-        for (auto &it : items)
-        {
-            pdf << it.sku << " x" << it.quantity << " @ " << it.unitPrice << "\n";
-        }
-        pdf << "Subtotal: " << subtotal << "\n"
-            << "Discounts: " << discount_total << "\n"
-            << "Tax: " << tax << "\n"
-            << "Total: " << grand << "\n";
-
         // email I/O inline (tight coupling)
         if (!email.empty())
         {
@@ -109,8 +160,6 @@ public:
 
         // logging inline
         cout << "[LOG] Invoice processed for " << email << " total=" << grand << "\n";
-
-        return pdf.str();
     }
 
     // helper used by ad-hoc tests; also messy on purpose
