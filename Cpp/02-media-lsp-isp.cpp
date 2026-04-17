7fcdf7ec8cd8070ffbe45ce18706d4ebaec8cf01
Pradeep4157 <krp18539@gmail.com>
2026-04-17 23:36:04 +0530

    done with 2nd..

--- Cpp/02-media-lsp-isp.cpp
+++ Cpp/02-media-lsp-isp.cpp
@@ -4,62 +4,82 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+/*
+    Player is really fat interface, we need to remove some of the function and create some more
 
+    interfacse to add these functions..
+
+    interface segregation is done..
+
+    in the play function there is a question of whether live stream has started or not ..
+
+
+
+*/
 using namespace std;
-
-class Player {
+class Playable
+{
 public:
-    virtual ~Player() = default;
-    virtual void play(const string& source) = 0;
+    virtual void play(const string &source) = 0;
     virtual void pause() = 0;
-    virtual void record(const string& destination) = 0;  // many can't
-    virtual void streamLive(const string& url) = 0;       // many can't
-    virtual void download(const string& sourceUrl) = 0;   // many can't
+};
+class Downloadable
+{
+public:
+    virtual void download(const string &sourceUrl) = 0;
+};
+class LiveStreamable
+{
+public:
+    virtual void streamLive(const string &url) = 0;
+};
+class VideoRecordable
+{
+public:
+    virtual void record(const string &destination) = 0;
 };
 
-class AudioPlayer : public Player {
-    bool playing{false};
-public:
-    void play(const string& source) override { (void)source; playing = true; }
-    void pause() override { playing = false; }
-    void record(const string&) override { throw logic_error("AudioPlayer cannot record"); }       // LSP break
-    void streamLive(const string&) override { throw logic_error("AudioPlayer cannot streamLive"); } // LSP break
-    void download(const string& url) override { (void)url; /* pretend */ }
-    bool isPlaying() const { return playing; }
-};
+class AudioPlayer : public Playable, public Downloadable
+{
 
-class CameraStreamPlayer : public Player {
-    bool liveStarted{false};
     bool playing{false};
+
 public:
-    void play(const string& src) override {
-        (void)src;
-        // Surprise: needs streamLive first for “real” play
-        if (!liveStarted) {
-            cout << "[WARN] playing without live stream started.\n";
-        }
+    void play(const string &source) override
+    {
+        (void)source;
         playing = true;
     }
     void pause() override { playing = false; }
-    void record(const string& dest) override { (void)dest; /* pretend */ }
-    void streamLive(const string& url) override { (void)url; liveStarted = true; }
-    void download(const string&) override { throw logic_error("CameraStreamPlayer cannot download"); } // LSP break
+    void download(const string &url) override { (void)url; /* pretend */ }
+    bool isPlaying() const { return playing; }
+};
+
+class CameraStreamPlayer : public LiveStreamable, public VideoRecordable
+{
+    bool liveStarted{false};
+    bool playing{false};
+
+public:
+    void record(const string &dest) override { (void)dest; /* pretend */ }
+    void streamLive(const string &url) override
+    {
+        (void)url;
+        liveStarted = true;
+    }
     bool isPlaying() const { return playing; }
     bool isLive() const { return liveStarted; }
 };
 
-int main() {
+int main()
+{
     AudioPlayer ap;
     ap.play("song.mp3");
     cout << "Audio playing: " << boolalpha << ap.isPlaying() << "\n";
     ap.pause();
 
     CameraStreamPlayer cam;
-    cam.play("rtsp://camera");       // warning surprise
     cam.streamLive("rtsp://camera"); // required order
-    cam.play("rtsp://camera");
-    try { cam.download("http://file"); } catch (const exception& e) {
-        cout << "[EXC] " << e.what() << "\n";
-    }
+
     return 0;
 }
--- Cpp/03-notify-dip-ocp.cpp
+++ Cpp/03-notify-dip-ocp.cpp
@@ -4,38 +4,76 @@
 
 using namespace std;
 
-class SmtpMailer {
+/*
+
+    OBSERVATIONS :
+
+        SMTPMailer looks good, it is just sending the mails,
+
+        twilioclient is also just sending otps..
+
+
+
+
+*/
+
+class SmtpMailer
+{
 public:
-    void send(const string& templ, const string& to, const string& body){
+    void send(const string &templ, const string &to, const string &body)
+    {
         cout << "[SMTP] template=" << templ << " to=" << to << " body=" << body << "\n";
     }
 };
-class TwilioClient {
+class TwilioClient
+{
 public:
-    void sendOTP(const string& phone, const string& code){
+    void sendOTP(const string &phone, const string &code)
+    {
         cout << "[Twilio] OTP " << code << " -> " << phone << "\n";
     }
 };
 
-struct User { string email; string phone; };
+struct User
+{
+    string email;
+    string phone;
+};
+void handleWelcomeMessage(const string &email, SmtpMailer &mailer)
+{
+    mailer.send("welcome", email, "Welcome!");
+}
+string generateOTP()
+{
+    int otp = rand() % 900000 + 100000;
 
-class SignUpService {
+    return to_string(otp);
+}
+void handleSendOTP(const string &phone, TwilioClient &sms)
+{
+    const string otp = generateOTP();
+    sms.sendOTP(phone, otp);
+}
+class SignUpService
+{
 public:
-    bool signUp(const User& u){
-        if (u.email.empty()) return false;
+    bool signUp(const User &u)
+    {
+        if (u.email.empty())
+            return false;
         // pretend DB save here…
 
         // hard-coded providers
         SmtpMailer mailer;
-        mailer.send("welcome", u.email, "Welcome!");
-
+        handleWelcomeMessage(u.email, mailer);
         TwilioClient sms;
-        sms.sendOTP(u.phone, "123456");
+        handleSendOTP(u.phone, sms);
         return true;
     }
 };
 
-int main() {
+int main()
+{
     SignUpService svc;
     svc.signUp({"user@example.com", "+15550001111"});
     return 0;
